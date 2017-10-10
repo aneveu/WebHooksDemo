72e94410a128af9027c485437622b0dc1aca8903
Audrey Neveu <neveu.audrey@gmail.com>
2017-10-10 10:22:44 +0200

    demo code

--- app.js
+++ app.js
@@ -15,3 +15,25 @@ var server = app.listen("8085", "localhost", function () {
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
+
+app.post('/webhook', function (req, res) {
+    var event = req.headers["x-github-event"];
+
+    switch (event) {
+
+        case "ping":
+            res.send("Ping event received");
+            console.log("Ping event received");
+            break;
+
+        case "watch":
+            res.send("Watch event received");
+            console.log("New watcher: ", req.body.sender.login);
+            break;
+
+        default:
+            res.status(400).send("Event not supported yet:", event);
+            console.log("Unsupported event: ", event);
+            break;
+    }
+});
